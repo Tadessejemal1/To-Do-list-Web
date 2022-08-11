5db6263081fcb618bd105e7b53a72688928fb105
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-11 10:12:30 +0300

    Fix lintr error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -51,8 +50,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Webpack App',
       filename: 'index.html',
-      template: "src/template.html",
+      template: "src/template.html"
     }),
     new BundleAnalyzerPlugin(),
   ],      
-}
+};
