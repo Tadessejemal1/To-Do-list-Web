39d5c98cb028fc9ea7a3b98e86b751273059229d
Tadesse Jemal <tadessejemal63@gmail.com>
2022-08-11 10:29:13 +0300

    Fix lint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
   mode: 'development',
@@ -44,14 +44,13 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack App',
       filename: 'index.html',
-      template: "src/template.html"
+      template: "src/template.html",
     }),
-    new BundleAnalyzerPlugin(),
-  ],      
+  ],
 };
